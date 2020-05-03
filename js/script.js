4826f1acad5ba57d41a4e0a61450f86d591b0886
ResmiRamani <Resmi.RamaniAmma@triosstudent.com>
2020-05-03 15:43:23 -0400

    First commit

---
+++ js/script.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+  $("#slides").superslides({
+    animation: "fade",
+    play: 5000,
+    pagination: false,
+  });
+
+  var typed = new Typed(".typed", {
+    strings: [
+      "Software Engineer.",
+      "Web Developer.",
+      "Application Developer.",
+      "Student.",
+    ],
+    typeSpeed: 70,
+    loop: true,
+    startDelay: 1000,
+    showCursor: false,
+  });
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    items: 4,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      480: {
+        items: 2,
+      },
+      768: {
+        items: 3,
+      },
+      938: {
+        items: 4,
+      },
+    },
+  });
+  var skillsTopOffset = $(".skillsSection").offset().top;
+  $(window).scroll(function () {
+    if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+      $(".chart").easyPieChart({
+        easing: "easeInOut",
+        barColor: "#fff",
+        trackColor: false,
+        scaleColor: false,
+        lineWidth: 4,
+        size: 152,
+        onStep: function (from, to, percent) {
+          $(this.el).find(".percent").text(Math.round(percent));
+        },
+      });
+    }
+  });
+});
