035002811576107aa9a1944076e2e5607671ec7d
ResmiRamani <Resmi.RamaniAmma@triosstudent.com>
2020-05-31 14:53:35 -0400

    Creating the navigation menu,Portfolio section,Copyright section and Page Loading Overlay

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+$(window).on("load", function () {
+  $(".loader .inner").fadeOut(500, function () {
+    $(".loader").fadeOut(750);
+  });
+
+  $(".items").isotope({
+    filter: "*",
+    animationOptions: {
+      duration: 1500,
+      easing: "linear",
+      queue: false,
+    },
+  });
+});
+
 $(document).ready(function () {
   $("#slides").superslides({
     animation: "fade",
@@ -6,17 +21,13 @@ $(document).ready(function () {
   });
 
   var typed = new Typed(".typed", {
-    strings: [
-      "Software Engineer.",
-      "Web Developer.",
-      "Application Developer.",
-      "Student.",
-    ],
+    strings: ["Software Engineer.", "Web Developer.", "Student."],
     typeSpeed: 70,
     loop: true,
     startDelay: 1000,
     showCursor: false,
   });
+
   $(".owl-carousel").owlCarousel({
     loop: true,
     items: 4,
@@ -35,6 +46,7 @@ $(document).ready(function () {
       },
     },
   });
+
   var skillsTopOffset = $(".skillsSection").offset().top;
   var statsTopOffset = $(".statsSection").offset().top;
   var countUpFinished = false;
@@ -63,7 +75,53 @@ $(document).ready(function () {
 
         element.countup(endVal);
       });
+
       countUpFinished = true;
     }
   });
+
+  $("[data-fancybox]").fancybox();
+
+  $("#filters a").click(function () {
+    $("#filters .current").removeClass("current");
+    $(this).addClass("current");
+
+    var selector = $(this).attr("data-filter");
+
+    $(".items").isotope({
+      filter: selector,
+      animationOptions: {
+        duration: 1500,
+        easing: "linear",
+        queue: false,
+      },
+    });
+
+    return false;
+  });
+
+  $("#navigation li a").click(function (e) {
+    e.preventDefault();
+
+    var targetElement = $(this).attr("href");
+    var targetPosition = $(targetElement).offset().top;
+    $("html, body").animate({ scrollTop: targetPosition - 50 }, "slow");
+  });
+
+  const nav = $("#navigation");
+  const navTop = nav.offset().top;
+
+  $(window).on("scroll", stickyNavigation);
+
+  function stickyNavigation() {
+    var body = $("body");
+
+    if ($(window).scrollTop() >= navTop) {
+      body.css("padding-top", nav.outerHeight() + "px");
+      body.addClass("fixedNav");
+    } else {
+      body.css("padding-top", 0);
+      body.removeClass("fixedNav");
+    }
+  }
 });
